d50cede6c9c08eb08079a0efa591e2133c36ad4b
RazerEdge64 <kohirkarshubhang@gmail.com>
2023-10-04 18:18:50 -0400

    ui overhaul

---
+++ public/kanbas/sidebars/sidebar.js
@@ -0,0 +1,17 @@
+function toggleCourseNav() {
+    const courseNav = document.getElementById('courseNav');
+    if (courseNav.style.display === 'none' || courseNav.style.display === '') {
+        courseNav.style.display = 'block';
+    } else {
+        courseNav.style.display = 'none';
+    }
+}
+
+function toggleProfileNav() {
+    const courseNav = document.getElementById('profileNav');
+    if (courseNav.style.display === 'none' || courseNav.style.display === '') {
+        courseNav.style.display = 'block';
+    } else {
+        courseNav.style.display = 'none';
+    }
+}
